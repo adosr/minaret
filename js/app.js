d3ab1f350a13a483c98d397226f456a38b9be5c2
adosr <adosr3@gmail.com>
2026-04-16 05:36:06 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,3 +5,13 @@ bootstrapApp().catch((error) => {
   document.documentElement.classList.remove("preinit");
   document.body?.classList?.remove("preinit");
 });
+
+window.addEventListener('DOMContentLoaded', () => {
+  const hash = window.location.hash.toLowerCase();
+
+  if (hash === '#monthly') {
+    document.getElementById('tabMonthly')?.click();
+  } else if (hash === '#daily') {
+    document.getElementById('tabDaily')?.click();
+  }
+});
